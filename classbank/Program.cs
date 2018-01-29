7ca6be9c5887fde4c8a45cc9760210dde86bdb17
DESKTOP-FSUTELI\tustr <tustrs@gmail.com>
2018-01-29 20:07:55 +0200

    Add project files.

---
+++ classbank/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classbank
+{
+    class Bank
+    {
+        public int Account { get; private set; }
+        private string fullName;
+        private int age;
+        private bool anyCredits;
+
+        public Bank(int account, string fullName, int age, bool anyCredits)
+        {
+            Account = account;
+            this.fullName = fullName;
+            this.age = age;
+            this.anyCredits = anyCredits;
+        }
+    }
+}
+
+
