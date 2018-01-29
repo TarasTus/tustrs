9b1a4af718cde19d304ce6c6c13a40aa8b9bb23c
DESKTOP-FSUTELI\tustr <tustrs@gmail.com>
2018-01-29 20:34:51 +0200

    hello q Please enter the commit message for your changes. Lines starting

---
+++ tustrs/credit card/Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace credit_card
+{
+    public class CreditCard
+    {
+        int rahunok;
+        int No;
+        int CWW;
+        string name;
+        public CreditCard(int rah, int no, int cww, string name)
+        {
+            this.rahunok = rah;
+            this.No = no;
+            this.CWW = cww;
+            this.name = name;
+        }
+        public CreditCard() { rahunok = 0; No = 0; CWW = 0; name = "no name"; }
+        int GetSetRah
+        {
+            get { return rahunok; }
+            set { rahunok = value; }
+        }
+        int GetSetNo
+        {
+            get { return No; }
+            set { No = value; }
+        }
+        int GetSetCWW
+        {
+            get { return CWW; }
+            set { CWW = value; }
+        }
+        string GetSetName
+        {
+            get { return name; }
+            set { name = value; }
+        }
+    }
+}
